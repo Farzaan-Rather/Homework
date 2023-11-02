ea8fa3a0dd674049e90f0b29e70857c5d6d56058
Farzaan-Rather <147377642+Farzaan-Rather@users.noreply.github.com>
2023-11-02 18:10:18 +0530

    Add files via upload

---
+++ Sum of first five number's and their square.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main () {
+int num;
+for(num=1;num<=5;num++);
+{
+    printf("%d\n",num);
+    int square=num*num;
+    printf("square%d\n",square);
+}
+printf("sum of five numbers  %d\n",1+2+3+4+5);
+printf("Sum of squares %d\n",1+4+9+16+25);
+return 0;
+
+
+
+
+
+}
