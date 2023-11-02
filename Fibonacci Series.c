9242c6613501b388ce88f72f909166cb7368e7e0
Farzaan-Rather <147377642+Farzaan-Rather@users.noreply.github.com>
2023-11-02 18:07:25 +0530

    Add files via upload

---
+++ Fibonacci Series.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void main () {
+    int i, f=0,s=1,n;
+    scanf("%d",&n);
+
+for(i=0;i<10;i++) {
+  printf("%d",f);
+  int next =  f+s;
+  f=s;
+  s=next;
+
+
+
+
+}
+
+return 0;
+
+
+}
